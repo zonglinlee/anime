a18d294886be77019727acf186de4defc993f7ca
Julian Garnier <hello@julian.gr>
2019-01-02 12:38:21 +0100

    Update documentation

--- documentation/assets/js/website.js
+++ documentation/assets/js/website.js
@@ -4,9 +4,9 @@ import anime from '../../../src/index.js';
 
 !function(t,e){"use strict";function n(t){this.time=t.time,this.target=t.target,this.rootBounds=t.rootBounds,this.boundingClientRect=t.boundingClientRect,this.intersectionRect=t.intersectionRect||a(),this.isIntersecting=!!t.intersectionRect;var e=this.boundingClientRect,n=e.width*e.height,i=this.intersectionRect,o=i.width*i.height;n?this.intersectionRatio=o/n:this.intersectionRatio=this.isIntersecting?1:0}function i(t,e){var n=e||{};if("function"!=typeof t)throw new Error("callback must be a function");if(n.root&&1!=n.root.nodeType)throw new Error("root must be an Element");this._checkForIntersections=r(this._checkForIntersections.bind(this),this.THROTTLE_TIMEOUT),this._callback=t,this._observationTargets=[],this._queuedEntries=[],this._rootMarginValues=this._parseRootMargin(n.rootMargin),this.thresholds=this._initThresholds(n.threshold),this.root=n.root||null,this.rootMargin=this._rootMarginValues.map(function(t){return t.value+t.unit}).join(" ")}function o(){return t.performance&&performance.now&&performance.now()}function r(t,e){var n=null;return function(){n||(n=setTimeout(function(){t(),n=null},e))}}function s(t,e,n,i){"function"==typeof t.addEventListener?t.addEventListener(e,n,i||!1):"function"==typeof t.attachEvent&&t.attachEvent("on"+e,n)}function h(t,e,n,i){"function"==typeof t.removeEventListener?t.removeEventListener(e,n,i||!1):"function"==typeof t.detatchEvent&&t.detatchEvent("on"+e,n)}function c(t,e){var n=Math.max(t.top,e.top),i=Math.min(t.bottom,e.bottom),o=Math.max(t.left,e.left),r=Math.min(t.right,e.right),s=r-o,h=i-n;return s>=0&&h>=0&&{top:n,bottom:i,left:o,right:r,width:s,height:h}}function u(t){var e;try{e=t.getBoundingClientRect()}catch(n){}return e?(e.width&&e.height||(e={top:e.top,right:e.right,bottom:e.bottom,left:e.left,width:e.right-e.left,height:e.bottom-e.top}),e):a()}function a(){return{top:0,bottom:0,left:0,right:0,width:0,height:0}}function l(t,e){for(var n=e;n;){if(n==t)return!0;n=p(n)}return!1}function p(t){var e=t.parentNode;return e&&11==e.nodeType&&e.host?e.host:e}if("IntersectionObserver"in t&&"IntersectionObserverEntry"in t&&"intersectionRatio"in t.IntersectionObserverEntry.prototype)return void("isIntersecting"in t.IntersectionObserverEntry.prototype||Object.defineProperty(t.IntersectionObserverEntry.prototype,"isIntersecting",{get:function(){return this.intersectionRatio>0}}));var f=[];i.prototype.THROTTLE_TIMEOUT=100,i.prototype.POLL_INTERVAL=null,i.prototype.observe=function(t){var e=this._observationTargets.some(function(e){return e.element==t});if(!e){if(!t||1!=t.nodeType)throw new Error("target must be an Element");this._registerInstance(),this._observationTargets.push({element:t,entry:null}),this._monitorIntersections(),this._checkForIntersections()}},i.prototype.unobserve=function(t){this._observationTargets=this._observationTargets.filter(function(e){return e.element!=t}),this._observationTargets.length||(this._unmonitorIntersections(),this._unregisterInstance())},i.prototype.disconnect=function(){this._observationTargets=[],this._unmonitorIntersections(),this._unregisterInstance()},i.prototype.takeRecords=function(){var t=this._queuedEntries.slice();return this._queuedEntries=[],t},i.prototype._initThresholds=function(t){var e=t||[0];return Array.isArray(e)||(e=[e]),e.sort().filter(function(t,e,n){if("number"!=typeof t||isNaN(t)||0>t||t>1)throw new Error("threshold must be a number between 0 and 1 inclusively");return t!==n[e-1]})},i.prototype._parseRootMargin=function(t){var e=t||"0px",n=e.split(/\s+/).map(function(t){var e=/^(-?\d*\.?\d+)(px|%)$/.exec(t);if(!e)throw new Error("rootMargin must be specified in pixels or percent");return{value:parseFloat(e[1]),unit:e[2]}});return n[1]=n[1]||n[0],n[2]=n[2]||n[0],n[3]=n[3]||n[1],n},i.prototype._monitorIntersections=function(){this._monitoringIntersections||(this._monitoringIntersections=!0,this.POLL_INTERVAL?this._monitoringInterval=setInterval(this._checkForIntersections,this.POLL_INTERVAL):(s(t,"resize",this._checkForIntersections,!0),s(e,"scroll",this._checkForIntersections,!0),"MutationObserver"in t&&(this._domObserver=new MutationObserver(this._checkForIntersections),this._domObserver.observe(e,{attributes:!0,childList:!0,characterData:!0,subtree:!0}))))},i.prototype._unmonitorIntersections=function(){this._monitoringIntersections&&(this._monitoringIntersections=!1,clearInterval(this._monitoringInterval),this._monitoringInterval=null,h(t,"resize",this._checkForIntersections,!0),h(e,"scroll",this._checkForIntersections,!0),this._domObserver&&(this._domObserver.disconnect(),this._domObserver=null))},i.prototype._checkForIntersections=function(){var t=this._rootIsInDom(),e=t?this._getRootRect():a();this._observationTargets.forEach(function(i){var r=i.element,s=u(r),h=this._rootContainsTarget(r),c=i.entry,a=t&&h&&this._computeTargetAndRootIntersection(r,e),l=i.entry=new n({time:o(),target:r,boundingClientRect:s,rootBounds:e,intersectionRect:a});c?t&&h?this._hasCrossedThreshold(c,l)&&this._queuedEntries.push(l):c&&c.isIntersecting&&this._queuedEntries.push(l):this._queuedEntries.push(l)},this),this._queuedEntries.length&&this._callback(this.takeRecords(),this)},i.prototype._computeTargetAndRootIntersection=function(n,i){if("none"!=t.getComputedStyle(n).display){for(var o=u(n),r=o,s=p(n),h=!1;!h;){var a=null,l=1==s.nodeType?t.getComputedStyle(s):{};if("none"==l.display)return;if(s==this.root||s==e?(h=!0,a=i):s!=e.body&&s!=e.documentElement&&"visible"!=l.overflow&&(a=u(s)),a&&(r=c(a,r),!r))break;s=p(s)}return r}},i.prototype._getRootRect=function(){var t;if(this.root)t=u(this.root);else{var n=e.documentElement,i=e.body;t={top:0,left:0,right:n.clientWidth||i.clientWidth,width:n.clientWidth||i.clientWidth,bottom:n.clientHeight||i.clientHeight,height:n.clientHeight||i.clientHeight}}return this._expandRectByRootMargin(t)},i.prototype._expandRectByRootMargin=function(t){var e=this._rootMarginValues.map(function(e,n){return"px"==e.unit?e.value:e.value*(n%2?t.width:t.height)/100}),n={top:t.top-e[0],right:t.right+e[1],bottom:t.bottom+e[2],left:t.left-e[3]};return n.width=n.right-n.left,n.height=n.bottom-n.top,n},i.prototype._hasCrossedThreshold=function(t,e){var n=t&&t.isIntersecting?t.intersectionRatio||0:-1,i=e.isIntersecting?e.intersectionRatio||0:-1;if(n!==i)for(var o=0;o<this.thresholds.length;o++){var r=this.thresholds[o];if(r==n||r==i||n>r!=i>r)return!0}},i.prototype._rootIsInDom=function(){return!this.root||l(e,this.root)},i.prototype._rootContainsTarget=function(t){return l(this.root||e,t)},i.prototype._registerInstance=function(){f.indexOf(this)<0&&f.push(this)},i.prototype._unregisterInstance=function(){var t=f.indexOf(this);-1!=t&&f.splice(t,1)},t.IntersectionObserver=i,t.IntersectionObserverEntry=n}(window,document);
 
-/* Draggable elements */
+/* Helpers */
 
-function draggable(el, events) {
+function dragElement(el, events) {
 
   function getPointer(e) {
     var x = 'clientX';
@@ -75,38 +75,45 @@ function isElementInViewport(el, inCB, outCB, rootMargin) {
   observer.observe(el);
 }
 
-function fitElToParent(el, padding) {
+function fitElementToParent(el, padding) {
+  var timeout = null;
   function resize() {
-    anime.setValue(el, {scale: 1});
+    if (timeout) clearTimeout(timeout);
+    anime.set(el, {scale: 1});
     var pad = padding || 0;
     var parentEl = el.parentNode;
     var elOffsetWidth = el.offsetWidth - pad;
     var parentOffsetWidth = parentEl.offsetWidth;
     var ratio = parentOffsetWidth / elOffsetWidth;
-    anime.setValue(el, {scale: ratio});
+    timeout = setTimeout(anime.set(el, {scale: ratio}), 10);
   }
   resize();
   window.addEventListener('resize', resize);
 }
 
+// Update date and version number
+
+var versionNumerEls = document.querySelectorAll('.version-number');
+var dateEl = document.querySelector('.date');
+var date = new Date();
+
+for (var i = 0; i < versionNumerEls.length; i++) {
+  versionNumerEls[i].innerHTML = anime.version;
+}
+dateEl.innerHTML = date.getFullYear();
+
+// main logo animation
+
 var logoAnimation = (function() {
 
   var logoAnimationEl = document.querySelector('.logo-animation');
   var bouncePath = anime.path('.bounce path');
-  var versionNumerEls = document.querySelectorAll('.version-number');
-  var dateEl = document.querySelector('.date');
-  var date = new Date();
 
-  for (var i = 0; i < versionNumerEls.length; i++) {
-    versionNumerEls[i].innerHTML = anime.version;
-  }
-  // dateEl.innerHTML = date.getFullYear();
+  fitElementToParent(logoAnimationEl, 0);
 
-  fitElToParent(logoAnimationEl, 0);
-
-  anime.setValue(['.letter-a', '.letter-n', '.letter-i'], {translateX: 70});
-  anime.setValue('.letter-e', {translateX: -70});
-  anime.setValue('.dot', { translateX: 595, translateY: -200 });
+  anime.set(['.letter-a', '.letter-n', '.letter-i'], {translateX: 70});
+  anime.set('.letter-e', {translateX: -70});
+  anime.set('.dot', { translateX: 630, translateY: -200 });
 
   var logoAnimationTL = anime.timeline({
     autoplay: false,
@@ -185,7 +192,7 @@ var logoAnimation = (function() {
     scale: 1,
     rotate: '1turn',
     scaleY: {value: .5, delay: 0, duration: 150, delay: 230},
-    translateX: 405,
+    translateX: 430,
     translateY: [
       {value: 244, duration: 100},
       {value: 204, duration: 200, delay: 130},
@@ -285,7 +292,7 @@ var introEasingsAnimation = (function() {
   var animation;
   var paused = true;
 
-  fitElToParent(easingVisualizerEl);
+  fitElementToParent(easingVisualizerEl);
 
   for (var i = 0; i < numberOfBars; i++) {
     var barEl = document.createElement('div');
@@ -412,7 +419,7 @@ var sphereAnimation = (function() {
   var hasStarted = false;
   var aimations = [];
 
-  fitElToParent(sphereEl);
+  fitElementToParent(sphereEl);
 
   var breathAnimation = anime({
     begin: function() {
@@ -497,7 +504,7 @@ var advancedStaggeringAnimation = (function() {
   var animation;
   var paused = true;
 
-  fitElToParent(staggerVisualizerEl);
+  fitElementToParent(staggerVisualizerEl);
 
   for (var i = 0; i < numberOfElements; i++) {
     var dotEl = document.createElement('div');
@@ -510,7 +517,7 @@ var advancedStaggeringAnimation = (function() {
   var index = anime.random(0, numberOfElements-1);
   var nextIndex = 0;
 
-  anime.setValue('.stagger-visualizer .cursor', {
+  anime.set('.stagger-visualizer .cursor', {
     translateX: anime.stagger(-cell, {grid: grid, from: index, axis: 'x'}),
     translateY: anime.stagger(-cell, {grid: grid, from: index, axis: 'y'}),
     translateZ: 0,
@@ -639,7 +646,7 @@ var timeControlAnimation = (function() {
     anim: null
   };
 
-  var drag = draggable(timeCursorEl, {
+  var drag = dragElement(timeCursorEl, {
     begin: function(e) {
       anime.remove(time);
       time.start = timelineAnimation.currentTime;
@@ -706,7 +713,7 @@ var timeControlAnimation = (function() {
 
   for (var i = 0; i < infoEls.length; i++) {
     var infoEl = infoEls[i];
-    var delay = parseFloat(anime.getValue(infoEl, 'data-delay'));
+    var delay = parseFloat(anime.get(infoEl, 'data-delay'));
     var direction = infoEl.classList.contains('info-bottom') ? -1 : 1;
     timelineAnimation
     .add({
@@ -753,7 +760,7 @@ var layeredAnimation = (function() {
   var trianglePoints = triangleEl.getAttribute('points').split(' ');
   var easings = ['easeInOutQuad', 'easeInOutCirc', 'easeInOutSine', 'spring'];
 
-  fitElToParent(layeredAnimationEl);
+  fitElementToParent(layeredAnimationEl);
 
   function createKeyframes(value) {
     var keyframes = [];
