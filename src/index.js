f814d5b5f3ca1f725062d517d7224b8ac4508252
Julian Garnier <julian.garnier@gmail.com>
2019-07-23 10:37:34 +0200

    Easings refactoring + add Bounce easing

--- src/index.js
+++ src/index.js
@@ -120,17 +120,6 @@ function spring(string, duration) {
 
 }
 
-// Elastic easing adapted from jQueryUI http://api.jqueryui.com/easings/
-
-function elastic(amplitude = 1, period = .5) {
-  const a = minMax(amplitude, 1, 10);
-  const p = minMax(period, .1, 2);
-  return t => {
-    return (t === 0 || t === 1) ? t : 
-      -a * Math.pow(2, 10 * (t - 1)) * Math.sin((((t - 1) - (p / (Math.PI * 2) * Math.asin(1 / a))) * (Math.PI * 2)) / p);
-  }
-}
-
 // Basic steps easing implementation https://developer.mozilla.org/fr/docs/Web/CSS/transition-timing-function
 
 function steps(steps = 10) {
@@ -222,55 +211,42 @@ const bezier = (() => {
 
 const penner = (() => {
 
-  const names = ['Quad', 'Cubic', 'Quart', 'Quint', 'Sine', 'Expo', 'Circ', 'Back', 'Elastic'];
+  // Based on jQuery UI's implemenation of easing equations from Robert Penner (http://www.robertpenner.com/easing)
 
-  // Approximated Penner equations http://matthewlein.com/ceaser/
+  const eases = { linear: () => t => t };
 
-  const curves = {
-    In: [
-      [0.550, 0.085, 0.680, 0.530], /* inQuad */
-      [0.550, 0.055, 0.675, 0.190], /* inCubic */
-      [0.895, 0.030, 0.685, 0.220], /* inQuart */
-      [0.755, 0.050, 0.855, 0.060], /* inQuint */
-      [0.470, 0.000, 0.745, 0.715], /* inSine */
-      [0.950, 0.050, 0.795, 0.035], /* inExpo */
-      [0.600, 0.040, 0.980, 0.335], /* inCirc */
-      [0.600,-0.280, 0.735, 0.045], /* inBack */
-      elastic /* inElastic */
-    ],
-    Out: [
-      [0.250, 0.460, 0.450, 0.940], /* outQuad */
-      [0.215, 0.610, 0.355, 1.000], /* outCubic */
-      [0.165, 0.840, 0.440, 1.000], /* outQuart */
-      [0.230, 1.000, 0.320, 1.000], /* outQuint */
-      [0.390, 0.575, 0.565, 1.000], /* outSine */
-      [0.190, 1.000, 0.220, 1.000], /* outExpo */
-      [0.075, 0.820, 0.165, 1.000], /* outCirc */
-      [0.175, 0.885, 0.320, 1.275], /* outBack */
-      (a, p) => t => 1 - elastic(a, p)(1 - t) /* outElastic */
-    ],
-    InOut: [
-      [0.455, 0.030, 0.515, 0.955], /* inOutQuad */
-      [0.645, 0.045, 0.355, 1.000], /* inOutCubic */
-      [0.770, 0.000, 0.175, 1.000], /* inOutQuart */
-      [0.860, 0.000, 0.070, 1.000], /* inOutQuint */
-      [0.445, 0.050, 0.550, 0.950], /* inOutSine */
-      [1.000, 0.000, 0.000, 1.000], /* inOutExpo */
-      [0.785, 0.135, 0.150, 0.860], /* inOutCirc */
-      [0.680,-0.550, 0.265, 1.550], /* inOutBack */
-      (a, p) => t => t < .5 ? elastic(a, p)(t * 2) / 2 : 1 - elastic(a, p)(t * -2 + 2) / 2 /* inOutElastic */
-    ]
+  const functionEasings = {
+    Sine: () => t => 1 - Math.cos(t * Math.PI / 2),
+    Circ: () => t => 1 - Math.sqrt(1 - t * t),
+    Back: () => t => t * t * (3 * t - 2),
+    Bounce: () => t => {
+      let pow2, b = 4;
+      while (t < (( pow2 = Math.pow(2, --b)) - 1) / 11) {};
+      return 1 / Math.pow(4, 3 - b) - 7.5625 * Math.pow(( pow2 * 3 - 2 ) / 22 - t, 2)
+    },
+    Elastic: (amplitude = 1, period = .5) => {
+      const a = minMax(amplitude, 1, 10);
+      const p = minMax(period, .1, 2);
+      return t => {
+        return (t === 0 || t === 1) ? t : 
+          -a * Math.pow(2, 10 * (t - 1)) * Math.sin((((t - 1) - (p / (Math.PI * 2) * Math.asin(1 / a))) * (Math.PI * 2)) / p);
+      }
+    }
   }
 
-  let eases = { 
-    linear: [0.250, 0.250, 0.750, 0.750]
-  }
+  const baseEasings = ['Quad', 'Cubic', 'Quart', 'Quint', 'Expo'];
 
-  for (let coords in curves) {
-    curves[coords].forEach((ease, i) => {
-      eases['ease'+coords+names[i]] = ease;
-    });
-  }
+  baseEasings.forEach((name, i) => {
+    functionEasings[name] = () => t => Math.pow(t, i + 2);
+  });
+
+  Object.keys(functionEasings).forEach(name => {
+    const easeIn = functionEasings[name];
+    eases['easeIn' + name] = easeIn;
+    eases['easeOut' + name] = (a, b) => t => 1 - easeIn(a, b)(1 - t);
+    eases['easeInOut' + name] = (a, b) => t => t < 0.5 ? easeIn(a, b)(t * 2) / 2 : 
+      1 - easeIn(a, b)(t * -2 + 2) / 2;
+  });
 
   return eases;
 
@@ -285,7 +261,7 @@ function parseEasings(easing, duration) {
     case 'spring' : return spring(easing, duration);
     case 'cubicBezier' : return applyArguments(bezier, args);
     case 'steps' : return applyArguments(steps, args);
-    default : return is.fnc(ease) ? applyArguments(ease, args) : applyArguments(bezier, ease);
+    default : return applyArguments(ease, args);
   }
 }
 
