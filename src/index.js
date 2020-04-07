6495820cd41d8bd623e3026e1411d3db8baede9a
kn0wn <admin@joshking.dev>
2020-04-07 18:35:30 +0100

    chore: bumped version number

--- src/index.js
+++ src/index.js
@@ -1242,7 +1242,7 @@ function timeline(params = {}) {
   return tl;
 }
 
-anime.version = '3.1.0';
+anime.version = '3.2.0';
 anime.speed = 1;
 anime.running = activeInstances;
 anime.remove = removeTargets;
