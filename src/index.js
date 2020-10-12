7edea3ef92a050d9c450e482da77343ccabad54d
Julian Garnier <julian.garnier@gmail.com>
2020-10-12 14:41:09 +0200

    Cleanup "is" helper functions

--- src/index.js
+++ src/index.js
@@ -56,12 +56,12 @@ const is = {
   str: a => typeof a === 'string',
   fnc: a => typeof a === 'function',
   und: a => typeof a === 'undefined',
+  nil: a => is.und(a) || a === null,
   hex: a => /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(a),
   rgb: a => /^rgb/.test(a),
   hsl: a => /^hsl/.test(a),
   col: a => (is.hex(a) || is.rgb(a) || is.hsl(a)),
   key: a => !defaultInstanceSettings.hasOwnProperty(a) && !defaultTweenSettings.hasOwnProperty(a) && a !== 'targets' && a !== 'keyframes',
-  nil: a => typeof a === 'undefined' || a === null
 }
 
 // Easings
