4b1c0dd40fab3538ebfe8bc87caf89cbbbb0514d
Julian Garnier <julian.garnier@gmail.com>
2021-04-15 14:45:17 +0200

    Cleanup getInstanceTimings and improve instance tests

--- tests/instances.test.js
+++ tests/instances.test.js
@@ -27,31 +27,43 @@ describe('Instances', () => {
     expect(instance01.animations).toEqual(expect.any(Array));
   });
 
-  test('Instances\'s animations timing inheritance', () => {
-    const instance = anime({
-      targets: '#target-id',
-      translateX: {
-        value: 50,
-        delay: 150,
-        duration: 100,
-        endDelay: 200
-      },
-      translateY: {
-        value: 350,
-        delay: 100,
-        duration: 100,
-        endDelay: 500
-      },
-      translateZ: {
-        value: 200,
-        delay: 350,
-        duration: 300,
-        endDelay: 400
-      },
-    });
+  const complexTimingsParams = {
+    targets: '#target-id',
+    translateX: {
+      value: 50,
+      delay: () => 150,
+      duration: () => 100,
+      endDelay: () => 200
+    },
+    translateY: {
+      value: 350,
+      delay: 100,
+      duration: 100,
+      endDelay: 500
+    },
+    translateZ: {
+      value: 200,
+      delay: 350,
+      duration: 300,
+      endDelay: 400
+    },
+    delay: () => 100,
+    duration: () => 100,
+    endDelay: () => 500
+  };
 
-    expect(instance.delay).toBe(100);
-    expect(instance.duration).toBe(1050);
-    expect(instance.endDelay).toBe(400);
+  test('Delay must be the smallest delay of the all the animations', () => {
+    const animation = anime(complexTimingsParams);
+    expect(animation.delay).toBe(100);
+  });
+
+  test('Duration must be the longest delay + duration of the all the animations', () => {
+    const animation = anime(complexTimingsParams);
+    expect(animation.duration).toBe(1050);
+  });
+
+  test('EndDelay must be the smallest endDelay from the the longest animation', () => {
+    const animation = anime(complexTimingsParams);
+    expect(animation.endDelay).toBe(400);
   });
 });
