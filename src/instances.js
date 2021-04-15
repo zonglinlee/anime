614debde973c08a7df6214c9d31e345250060eda
Julian Garnier <julian.garnier@gmail.com>
2021-04-15 12:08:13 +0200

    Fix getInstanceTimings

--- src/instances.js
+++ src/instances.js
@@ -24,28 +24,26 @@ let instancesId = 0;
 
 export function getInstanceTimings(animations, tweenSettings) {
   const animLength = animations.length;
-  const timings = {
+
+  if (!animLength) return {
     delay: tweenSettings.delay,
     duration: tweenSettings.duration,
     endDelay: tweenSettings.endDelay
   }
 
-  if (!animLength) return timings;
+  const timings = {};
 
   for (let i = 0; i < animLength; i++) {
     const anim = animations[i];
     const tlOffset = anim.timelineOffset;
-
     const delay = tlOffset + anim.delay;
     if (!timings.delay || delay < timings.delay) {
       timings.delay = delay;
     }
-
     const duration = tlOffset + anim.duration;
     if (!timings.duration || duration > timings.duration) {
       timings.duration = duration;
     }
-
     const endDelay = tlOffset + anim.duration - anim.endDelay;
     if (!timings.endDelay || endDelay > timings.endDelay) {
       timings.endDelay = endDelay;
