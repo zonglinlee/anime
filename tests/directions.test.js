1017115e848f35ddc53c9e5c99912c518cbbe887
Julian Garnier <julian.garnier@gmail.com>
2021-04-23 15:22:40 +0200

    Update directions tests

--- tests/directions.test.js
+++ tests/directions.test.js
@@ -1,15 +1,38 @@
 describe('Directions', () => {
   test('Forward', async resolve => {
-    let currentTime = 0;
     const animation = anime({
       targets: '#target-id',
       translateX: 100,
       direction: 'normal',
-      update: a => {
-        currentTime = a.currentTime;
-      },
       complete: (a) => {
         expect(a.progress).toEqual(1);
+        resolve();
+      },
+    });
+  });
+
+  test('Reverse', async resolve => {
+    const animation = anime({
+      targets: '#target-id',
+      translateX: 100,
+      direction: 'reverse',
+      complete: (a) => {
+        expect(a.progress).toEqual(0);
+        resolve();
+      },
+    });
+  });
+
+  test('Alternate', async resolve => {
+    const animation = anime({
+      targets: '#target-id',
+      translateX: 100,
+      direction: 'alternate',
+      loopComplete: (a) => {
+        expect(a.progress).toEqual(a.remainingLoops ? 1 : 0);
+      },
+      complete: (a) => {
+        expect(a.progress).toEqual(0);
         resolve();
       },
     });
