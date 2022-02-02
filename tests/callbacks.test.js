0349d2cf5605701431d2ee2f0cfec0ef49b69046
Julian Garnier <julian.garnier@gmail.com>
2022-02-02 17:18:22 +0100

    Add change() callback tests

--- tests/callbacks.test.js
+++ tests/callbacks.test.js
@@ -1,16 +1,29 @@
+function setupAnimationCallBack(callbackName, callbackFunc) {
+  const parameters = {
+    targets: '#target-id',
+    translateX: 100,
+    autoplay: false,
+    delay: 10,
+    endDelay: 10,
+    duration: 80,
+  }
+  parameters[callbackName] = callbackFunc;
+  return parameters;
+}
+
+function setupTimelineCallBack(callbackName, callbackFunc) {
+  const parameters = {
+    autoplay: false,
+  }
+  parameters[callbackName] = callbackFunc;
+  return parameters;
+}
+
 describe('Callbacks', () => {
   test('begin() on single instance', () => {
     let callbackCheck = false;
 
-    const animation = anime({
-      targets: '#target-id',
-      translateX: 100,
-      autoplay: false,
-      duration: 100,
-      begin: () => {
-        callbackCheck = true;
-      }
-    });
+    const animation = anime(setupAnimationCallBack('begin', () => { callbackCheck = true; }));
 
     expect(callbackCheck).toBe(false);
     animation.seek(50);
@@ -24,30 +37,9 @@ describe('Callbacks', () => {
     let tlAnim1CallbackCheck = false;
     let tlAnim2CallbackCheck = false;
 
-    const timeline = anime.timeline({
-      autoplay: false,
-      begin: () => {
-        tlCallbackCheck = true;
-      }
-    })
-    .add({
-      targets: '#target-id',
-      translateX: 100,
-      autoplay: false,
-      duration: 100,
-      begin: () => {
-        tlAnim1CallbackCheck = true;
-      }
-    })
-    .add({
-      targets: '#target-id',
-      translateY: 100,
-      autoplay: false,
-      duration: 100,
-      begin: () => {
-        tlAnim2CallbackCheck = true;
-      }
-    })
+    const timeline = anime.timeline(setupAnimationCallBack('begin', () => { tlCallbackCheck = true; }))
+    .add(setupAnimationCallBack('begin', () => { tlAnim1CallbackCheck = true; }))
+    .add(setupAnimationCallBack('begin', () => { tlAnim2CallbackCheck = true; }))
 
     expect(tlCallbackCheck).toBe(false);
     expect(tlAnim1CallbackCheck).toBe(false);
@@ -69,15 +61,7 @@ describe('Callbacks', () => {
   test('complete() on single instance', () => {
     let callbackCheck = false;
 
-    const animation = anime({
-      targets: '#target-id',
-      translateX: 100,
-      autoplay: false,
-      duration: 100,
-      complete: () => {
-        callbackCheck = true;
-      }
-    });
+    const animation = anime(setupAnimationCallBack('complete', () => { callbackCheck = true; }));
 
     expect(callbackCheck).toBe(false);
     animation.seek(50);
@@ -93,30 +77,9 @@ describe('Callbacks', () => {
     let tlAnim1CallbackCheck = false;
     let tlAnim2CallbackCheck = false;
 
-    const timeline = anime.timeline({
-      autoplay: false,
-      complete: () => {
-        tlCallbackCheck = true;
-      }
-    })
-    .add({
-      targets: '#target-id',
-      translateX: 100,
-      autoplay: false,
-      duration: 100,
-      complete: () => {
-        tlAnim1CallbackCheck = true;
-      }
-    })
-    .add({
-      targets: '#target-id',
-      translateY: 100,
-      autoplay: false,
-      duration: 100,
-      complete: () => {
-        tlAnim2CallbackCheck = true;
-      }
-    })
+    const timeline = anime.timeline(setupAnimationCallBack('complete', () => { tlCallbackCheck = true; }))
+    .add(setupAnimationCallBack('complete', () => { tlAnim1CallbackCheck = true; }))
+    .add(setupAnimationCallBack('complete', () => { tlAnim2CallbackCheck = true; }))
 
     expect(tlCallbackCheck).toBe(false);
     expect(tlAnim1CallbackCheck).toBe(false);
@@ -134,6 +97,54 @@ describe('Callbacks', () => {
     expect(tlAnim1CallbackCheck).toBe(true);
     expect(tlAnim2CallbackCheck).toBe(false);
     timeline.seek(200);
+    expect(tlCallbackCheck).toBe(true);
+    expect(tlAnim1CallbackCheck).toBe(true);
+    expect(tlAnim2CallbackCheck).toBe(true);
+  });
+
+  test('change() on single instance', () => {
+    let callbackCheck = false;
+    let changes = 0;
+
+    const animation = anime(setupAnimationCallBack('change', () => { changes++; callbackCheck = true; }));
+
+    expect(callbackCheck).toBe(false);
+    animation.seek(5);
+    expect(changes).toBe(0);
+    expect(callbackCheck).toBe(false);
+    animation.seek(10); // delay: 10
+    expect(changes).toBe(0);
+    expect(callbackCheck).toBe(false);
+    animation.seek(15);
+    expect(callbackCheck).toBe(true);
+    expect(changes).toBe(1);
+  });
+
+  test('change() in timeline', () => {
+    let tlCallbackCheck = false;
+    let tlAnim1CallbackCheck = false;
+    let tlAnim2CallbackCheck = false;
+
+    const timeline = anime.timeline(setupAnimationCallBack('change', () => { tlCallbackCheck = true; }))
+    .add(setupAnimationCallBack('change', () => { tlAnim1CallbackCheck = true; }))
+    .add(setupAnimationCallBack('change', () => { tlAnim2CallbackCheck = true; }))
+
+    expect(tlCallbackCheck).toBe(false);
+    expect(tlAnim1CallbackCheck).toBe(false);
+    expect(tlAnim2CallbackCheck).toBe(false);
+    timeline.seek(5);
+    expect(tlCallbackCheck).toBe(false);
+    expect(tlAnim1CallbackCheck).toBe(false);
+    expect(tlAnim2CallbackCheck).toBe(false);
+    timeline.seek(10); // delay: 10
+    expect(tlCallbackCheck).toBe(false);
+    expect(tlAnim1CallbackCheck).toBe(false);
+    expect(tlAnim2CallbackCheck).toBe(false);
+    timeline.seek(50);
+    expect(tlCallbackCheck).toBe(true);
+    expect(tlAnim1CallbackCheck).toBe(true);
+    expect(tlAnim2CallbackCheck).toBe(false);
+    timeline.seek(150);
     expect(tlCallbackCheck).toBe(true);
     expect(tlAnim1CallbackCheck).toBe(true);
     expect(tlAnim2CallbackCheck).toBe(true);
